2d24862d360ea86e5d0bdda077cc22fd85af9ab5
Miki <mpokrya@gmail.com>
2016-10-05 14:20:07 -0400

    Initial commit

---
+++ app/src/main/java/com/android/miki/rpiplantsapp/TempUnit.java
@@ -0,0 +1,16 @@
+package com.android.miki.rpiplantsapp;
+
+/**
+ * Created by Miki on 10/5/2016.
+ */
+public interface TempUnit {
+
+
+    class Celcius implements TempUnit {
+
+    }
+
+    class Fahrenheit implements TempUnit {
+
+    }
+}

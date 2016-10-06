b917c84b505bcc339d2be1d3f0d1cd89851b2b6f
Miki <mpokrya@gmail.com>
2016-10-06 11:42:29 -0400

    Overrode some methods in TempUnit. Added String tempUnit to its classes.

--- app/src/main/java/com/android/miki/rpiplantsapp/TempUnit.java
+++ app/src/main/java/com/android/miki/rpiplantsapp/TempUnit.java
@@ -4,13 +4,33 @@ package com.android.miki.rpiplantsapp;
  * Created by Miki on 10/5/2016.
  */
 public interface TempUnit {
-
+    double convertUnit(double value);
 
     class Celcius implements TempUnit {
-
+        private static final String TEMP_UNIT = "°C";
+        /**
+         * Converts value to Celcius.
+         * @param value Value to be converted.
+         * @return Converted value.
+         */
+        public double convertUnit(double value){
+            double convertedValue = ((value-32)*5)/9;
+            double roundedvalue = Math.round(convertedValue*100.0)/100.0;
+            return roundedvalue;
+        }
     }
 
     class Fahrenheit implements TempUnit {
-
+        private static final String TEMP_UNIT = "°F";
+        /**
+         * Converts value to Fahrenheit.
+         * @param value Value to be converted.
+         * @return Converted value.
+         */
+        public double convertUnit(double value){
+            double convertedValue = ((9*value)/5)+32;
+            double roundedvalue = Math.round(convertedValue*100)/100;
+            return roundedvalue;
+        }
     }
 }
